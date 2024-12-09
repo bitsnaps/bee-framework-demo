aff2f132dbe6c1234c6e25e0cbaae8a235cbee41
bhalouane <bhalouane@yahoo.fr>
2024-12-09 17:55:24 +0000

    improve README

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -19,7 +19,7 @@
       onCreate = {
         npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "index.html" "main.js" ];
+        default.openFiles = [ "lesson01.js" ];
       };
       # To run something each time the workspace is (re)started, use the `onStart` hook
     };
@@ -28,7 +28,7 @@
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT"];
+          command = ["npm" "run" "start"];
           manager = "web";
         };
       };
--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Bee Agent Framework Demo
 
-This is a simple example to use BeeAgentFramework by IBM.
+This is a simple example to use BeeAgentFramework created by IBM.
+This work is heavly inspired from [leonvanzyl](https://github.com/leonvanzyl/bee-agent-framework-tutorial) work.
 
 ## Bundle size:
 About ~ 100Mb.
+
+## Usage:
+Clone the repo then set your [Groq](https://console.groq.com/keys) API key in the `.env` file:
+```
+cp example.env .env
+```
+Install dependencies:
+```
+npm install
+```
+Checkout demos:
+```
+npm run start
+# or:
+npm run start:demo1
+
+npm run start:demo2
+npm run start:demo3
+```
+
